fe62af57ca0287956b9c1c0ac7dcdc5a2895cc8e
alextf21 <46171504+alextf21@users.noreply.github.com>
2026-02-17 13:25:16 +0100

    Add challenges and context

--- Real World Projects/CT Drug Overdoses/CT_Drug_Overdose_Analysis.sql
+++ Real World Projects/CT Drug Overdoses/CT_Drug_Overdose_Analysis.sql
@@ -58,10 +58,10 @@ LIMIT 10
 /* Intermediate SQL Challenges (Logic, Case Statements, Multi-Column Analysis) */
 
 -- Fentanyl Trend
--- For each year calculate
+-- For each year calculate:
 -- Total deaths
--- Deaths involving Fenatnyl
--- Percentage involving fentanyl
+-- Count of deaths where fentanyl is present
+-- Percentage of deaths where fentanyl is present
 SELECT 
   EXTRACT(year FROM Date) AS Year,
   COUNT(*) AS TotalDeaths,
@@ -83,6 +83,20 @@ GROUP BY Year, Sex
 HAVING Sex = 'Male' OR Sex = 'Female'
 ORDER BY Year ASC, Sex ASC
 
+-- Create a query that outputs per year:
+-- Total Deaths
+-- % fentanyl detected
+-- Average age
+SELECT
+  EXTRACT(year FROM Date) AS Year,
+  COUNT(*) TotalDeaths,
+  ROUND(
+    (COUNT(*) FILTER (WHERE Fentanyl = 'Y') / COUNT(*))  * 100, 2) AS FentanylDeathsPct,
+  ROUND(AVG(Age), 1) AS Age,
+FROM drug
+GROUP BY Year
+ORDER BY Year ASC
+
 /* Advanced SQL Challenges (Analytics, Risk Profiling, Trend Analysis) */
 
 -- Rolling 3-year death average
@@ -94,3 +108,59 @@ SELECT
     OVER(ORDER BY Year ROWS BETWEEN 3 PRECEDING AND CURRENT ROW), 2) AS Rolling3YearAverage
 FROM drug
 GROUP BY Year
+
+-- Emerging drug detection
+-- Determine the first year where:
+-- Xylazine was present in 5% of total deaths
+-- Answer: After 2019, every year has > 5%
+SELECT 
+  EXTRACT(year FROM Date) AS Year,
+  COUNT(*) AS Deaths,
+  COUNT(*) FILTER (WHERE Xylazine = 'Y') AS XylazineCount,   
+  ROUND(
+    (COUNT(*) FILTER (WHERE Xylazine = 'Y') / COUNT(*)) * 100, 2) AS PctXylazine
+FROM drug
+GROUP BY Year
+ORDER BY Year ASC
+
+-- Compare dominant substances prevalent in toxicology report for:
+-- Under 35
+-- 35 - 54
+-- 55+ 
+-- Below is for all data regardless of year
+SELECT
+  
+-- Break ages into groups
+  CASE 
+    WHEN Age < 35 THEN 'Under 35'
+    WHEN Age BETWEEN 35 AND 54 THEN '35-54'
+    WHEN Age >= 55 THEN '55+'
+    ELSE 'Unknown'
+  END AS AgeGroup,
+
+  -- Calculate counts for popular drugs for all years
+  SUM(CASE WHEN Heroin = 'Y' THEN 1 ELSE 0 END) AS HeroinCount,
+  SUM(CASE WHEN Cocaine = 'Y' THEN 1 ELSE 0 END) AS CocaineCount,
+  SUM(CASE WHEN Fentanyl = 'Y' THEN 1 ELSE 0 END) AS FentanylCount,
+  SUM(CASE WHEN Ethanol = 'Y' THEN 1 ELSE 0 END) AS EthanolCount,
+  SUM(CASE WHEN Benzodiazepine = 'Y' THEN 1 ELSE 0 END) AS BenzoCount,
+  SUM(CASE WHEN "Heroin/Morph/Codeine" = 'Y' THEN 1 ELSE 0 END) AS "Heroin/Morph/Codeine",
+
+  -- Total the above counts
+  HeroinCount + CocaineCount + FentanylCount + EthanolCount + BenzoCount AS Total 
+FROM drug
+WHERE Age IS NOT NULL
+GROUP BY AgeGroup
+
+-- Calculate YoY percentage change in deaths by year
+SELECT
+  EXTRACT(year FROM Date) AS Year,
+  COUNT(*) AS Deaths,
+  -- Identify difference between previous year deaths using the LAG() window function
+  Deaths - LAG(Deaths) OVER(ORDER BY Year) AS DeathsDiff,
+  -- Use above figure to divide by last years total death count and obtain YoY growth 
+  ROUND(
+    (DeathsDiff / LAG(Deaths) OVER(ORDER BY Year)) * 100, 2) AS YoYDifferencePct
+FROM drug
+GROUP BY Year
+ORDER BY Year ASC
